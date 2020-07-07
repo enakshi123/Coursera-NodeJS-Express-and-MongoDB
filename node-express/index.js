f73548bb3a45e70cd7f652e97b9cd941f8d5e5ce
enakshi123 <116.enakshi@gmail.com>
2020-07-07 15:31:36 +0530

    Express Serve Static Files

--- node-express/index.js
+++ node-express/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 //third party module gets automatically from node modules
 const http = require('http'); //core module
-
+const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express(); //application is gng to use express from node module 
 
+app.use(morgan('dev')); //development version
+
+app.use(express.static(__dirname + '/public'));
+//serve html files from public folder(root for files dirname)
+
 app.use((req, res, next) => {
-  console.log(req.headers);
+//   console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
